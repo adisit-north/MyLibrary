cce1a440761961ecc3ae15fd1870fd8d5d4cb9f5
Adisit <dorakengi@gmail.com>
2012-05-27 15:17:20 +0700

    init..

--- Badge_IOS_Style/src/com/adisit/badge_ios/Badge_IOS_StyleActivity.java
+++ Badge_IOS_Style/src/com/adisit/badge_ios/Badge_IOS_StyleActivity.java
@@ -9,5 +9,7 @@ public class Badge_IOS_StyleActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
     }
 }
