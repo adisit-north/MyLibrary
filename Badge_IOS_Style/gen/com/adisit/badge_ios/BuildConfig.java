286651caa6eeeb44ce5a341af894847883888600
Adisit <dorakengi@gmail.com>
2012-05-27 14:58:31 +0700

    Init Add Badge_IOS_Style.

---
+++ Badge_IOS_Style/gen/com/adisit/badge_ios/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.adisit.badge_ios;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Badge_IOS_Style/src/com/adisit/badge_ios/Badge_IOS_StyleActivity.java
@@ -0,0 +1,13 @@
+package com.adisit.badge_ios;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Badge_IOS_StyleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
